24538a16e6100ba4ab2c340b73e07159afb1661a
Brandon Levy <brandon.levy@mail.mcgill.ca>
2017-11-02 18:32:21 -0400

    completed permutation total

--- permutation_totals.rb
+++ permutation_totals.rb
@@ -1,12 +1,26 @@
 permutation = []
 combo =[]
-
+total =[]
 until permutation.length == 36
   dice_1 = Random.rand(1..6)
   dice_2 = Random.rand(1..6)
   combo = [dice_1, dice_2]
+  # combo = combo.join(" ")
   unless permutation.include? combo
   permutation << combo
   end
 end
-print permutation.sort
+permutation = permutation.sort
+# print permutation
+permutation.each do |combo|
+  sum = combo[0] + combo [1]
+  combo = combo.join(", ")
+  # puts combo
+  total << sum
+end
+
+i = 0
+until i == permutation.length
+  puts "Dice Roll: #{permutation[i].join(", ")}  Total: #{total[i]}"
+  i += 1
+end
--- permutations.rb
+++ permutations.rb
@@ -5,8 +5,10 @@ until permutation.length == 36
   dice_1 = Random.rand(1..6)
   dice_2 = Random.rand(1..6)
   combo = [dice_1, dice_2]
+  combo = combo.join(" ")
   unless permutation.include? combo
   permutation << combo
   end
 end
-print permutation.sort
+print permutation.sort.join("\n")
+puts
