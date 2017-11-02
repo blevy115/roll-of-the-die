dc89b46abcc630762d2e7bae9658319be12e4329
Brandon Levy <brandon.levy@mail.mcgill.ca>
2017-11-02 17:25:09 -0400

    completed doubles

---
+++ doubles.rb
@@ -0,0 +1,10 @@
+dice_1 = Random.rand(1..6)
+dice_2 = Random.rand(1..6)
+
+dice_total = dice_1 + dice_2
+
+puts "You rolled a #{dice_1} and #{dice_2}."
+if dice_1 == dice_2
+  puts "Doubles!"
+end
+puts "Your total is #{dice_total}."
