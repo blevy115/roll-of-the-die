d11e15b0fe62713fa38ef139c08ec47593e97388
Brandon Levy <brandon.levy@mail.mcgill.ca>
2017-11-02 21:40:59 -0400

    Added Dice side choice and RNG run count

--- total_times.rb
+++ total_times.rb
@@ -2,13 +2,16 @@ permutation = []
 combo =[]
 total =[]
 a = 1
-b = 6
-dice = Random.rand(a..b)
-
-until permutation.length == b**2
+puts "How many sides does Dice A have?"
+b = gets.chomp.to_i
+puts "How many sides does Dice B have?"
+c = gets.chomp.to_i
+n = 0
+until permutation.length == b*c
   dice_1 = Random.rand(a..b)
-  dice_2 = Random.rand(a..b)
+  dice_2 = Random.rand(a..c)
   combo = [dice_1, dice_2]
+  n += 1
   # combo = combo.join(" ")
   unless permutation.include? combo
   permutation << combo
@@ -23,12 +26,13 @@ permutation.each do |combo|
   total << sum
 end
 #
-# i = 0
-# until i == permutation.length
-#   puts "Dice Roll: #{permutation[i].join(", ")}  Total: #{total[i]}"
-#   i += 1
-# end
-# puts "There are #{permutation.length} possible permuations."
+i = 0
+puts "Dice Roll: A, B"
+until i == permutation.length
+  puts "Dice Roll: #{permutation[i].join(", ")}  Total: #{total[i]}"
+  i += 1
+end
+
 
 total_times = Hash.new 0
 total.each do |sum|
@@ -38,3 +42,5 @@ end
 total_times.each do |total, times|
   puts "#{total} occurs #{times} times"
 end
+puts "There are #{permutation.length} possible permuations."
+puts "This program required #{n} Randon Number Generator runs to complete."
